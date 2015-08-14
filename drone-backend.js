29c489656bda6109e4fc981e8e73bcb639f9ac73
V5792 (Course ID) <v5792@cam.ac.uk>
2015-08-14 11:29:32 +0100

    day 5

--- drone-backend.js
+++ drone-backend.js
@@ -14,8 +14,33 @@ Cylon.robot()
     .on("ready", fly);
     
 // Fly the bot
+
 function fly(robot) {
-
+    bot = robot;
+    bot.drone.disableEmergency();
+    bot.drone.ftrim();
+    bot.drone.takeoff(4 * 1000);
+    after(9 * 1000, function () {
+        bot.drone.land();
+    });
+    after(15 * 1000, function () {
+        bot.drone.stop();
+    });
+    bot.drone.left(0.2);
+    after(1 * 1000, function () {
+        bot.drone.left(0)
+    })
+    bot.drone.forward(0.2);
+    after(4 * 1000, function () {
+        bot.drone.forward(0)
+    })
+    bot.drone.right(0.2);
+    after(1 * 1000, function () {
+        bot.drone.right(0)
+    })
+    bot.drone.back(0.35);
+    after(2 * 1000, function () {
+        bot.drone.back(0)
+    })
 }
-
-Cylon.start();
+    Cylon.start();
